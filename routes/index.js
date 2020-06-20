09c566816ff6edc521026d3b6a0643ebd2848447
Luiz Mendes <developer.luizmendes@gmail.com>
2020-06-20 18:06:00 -0300

    teste deploy

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const MongoClient = require("mongodb").MongoClient;
+const CONNECTION_URL = "mongodb+srv://luizmendes:Pass1234@cluster0-c1rpf.mongodb.net/comentario?retryWrites=true&w=majority";
+const DATABASE_NAME = "comentario";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("asdasd");
-  // res.render('index', { title: 'Express' });
+var c
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+  if(error) {
+      throw error;
+  }
+  database = client.db(DATABASE_NAME);
+  collection = database.collection("comentario");
+
+
+
+  database.collection("comentarios").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result)
+  });
+
+  // console.log(a.ReadableState);
+  // res.send(a.ReadableState)
+});
+
+
+
+
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   var query = { address: "Park Lane 38" };
+//   dbo.collection("customers").find(query).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+  // });
+// });
+
 });
 
 module.exports = router;
