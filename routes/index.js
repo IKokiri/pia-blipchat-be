d5768b8e30dda03a24b122c4016a87246d16d51b
Luiz Mendes <developer.luizmendes@gmail.com>
2020-06-20 16:04:25 -0300

    teste deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res("asdasd");
+  res.send("asdasd");
   // res.render('index', { title: 'Express' });
 });
 
