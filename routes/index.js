57de80357b088da310a426c6725a9700a6992fac
Luiz Mendes <developer.luizmendes@gmail.com>
2020-06-20 18:33:42 -0300

    teste deploy

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 const MongoClient = require("mongodb").MongoClient;
-const CONNECTION_URL = "mongodb+srv://luizmendes:Pass1234@cluster0-c1rpf.mongodb.net/comentario?retryWrites=true&w=majority";
-const DATABASE_NAME = "comentario";
+const DATABASE_NAME = "piablipchat";
+const CONNECTION_URL = "mongodb+srv://luizmendes:Pass1234@cluster0-c1rpf.mongodb.net/"+DATABASE_NAME+"?retryWrites=true&w=majority";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,11 +13,11 @@ MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) =
       throw error;
   }
   database = client.db(DATABASE_NAME);
-  collection = database.collection("comentario");
+  collection = database.collection("custom");
 
 
 
-  database.collection("comentarios").find({}).toArray(function(err, result) {
+  database.collection("custom").find({}).toArray(function(err, result) {
     if (err) throw err;
     console.log(result);
     res.send(result)
